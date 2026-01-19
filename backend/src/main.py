5f214d00288c524bc628f5adc6cad3ae4ca4fa7d
Vinicius Boroto <viniciusdosanjos63@gmail.com>
2026-01-19 09:37:34 -0300

    feat: cors

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .database.database import create_tables
 from .api.routes import router, brand_router
 
 app = FastAPI(title="Whey Protein API", version="1.0.0")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.on_event("startup")
 def startup_event():
